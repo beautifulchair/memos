06f3170b4dac601eee1a2eaba555df1f08e407b0
meguro <beautifulchair13@gmail.com>
2023-02-05 00:15:44 +0900

    Inline styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,8 +24,8 @@ export default function Home() {
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main>
+        <div>
           <p>Hello</p>
           <button onClick={(e) => addNote(noteItems)} className=""></button>
         </div>

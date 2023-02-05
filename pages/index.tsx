bdd962822b120c54b762ee79709f5719e6692efb
meguro <beautifulchair13@gmail.com>
2023-02-05 21:39:16 +0900

    Implement event handlers

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,20 +35,83 @@ export default function Home() {
     console.log(noteItems);
   }
 
+  function keyDownOnTitle(
+    e: React.KeyboardEvent<HTMLTextAreaElement>,
+    item: NoteItem
+  ) {
+    if (e.key === "Enter") {
+      // invalidiate new line
+      e.preventDefault();
+
+      const id: number = item.id;
+      const newItem = noteItems.filter((v) => v.id === id)[0];
+      const target = e.currentTarget;
+      newItem.title = target.value;
+      const newNoteItems = noteItems.map((v) =>
+        v.id === item.id ? newItem : v
+      );
+      setNoteItems(newNoteItems);
+    }
+  }
+
+  function keyDownOnExplanation(
+    e: React.KeyboardEvent<HTMLTextAreaElement>,
+    item: NoteItem
+  ) {
+    const target: HTMLTextAreaElement = e.currentTarget;
+
+    if (e.key === "Enter" && !e.shiftKey) {
+      // invalidiate new line
+      e.preventDefault();
+
+      const id: number = item.id;
+      const newItem = noteItems.filter((v) => v.id === id)[0];
+      newItem.explanation = target.value;
+      const newNoteItems = noteItems.map((v) =>
+        v.id === item.id ? newItem : v
+      );
+      setNoteItems(newNoteItems);
+    } else if (e.key === "Enter" && e.shiftKey) {
+      const lineN = target.rows;
+      target.rows += 1;
+    }
+  }
+
+  function keyDownCaputureOnExplanation(
+    e: React.KeyboardEvent<HTMLTextAreaElement>
+  ) {
+    const target: HTMLTextAreaElement = e.currentTarget;
+    const lastChar = target.value.at(-1);
+    if (e.key === "Backspace" && lastChar?.charCodeAt(0) === 10) {
+      target.rows -= 1;
+    }
+  }
+
   const NoteTable = () => (
     <ol className="list-decimal mt-10">
       {noteItems.map((item) => (
         <li key={item.id} className="mt-7 border-b-2 border-dashed pb-3">
           <div className="">
             <div className="flex justify-between">
-              <p className="underline font-bold italic">{item.title}</p>
+              <textarea
+                className="underline font-bold italic"
+                rows={1}
+                defaultValue={item.title}
+                onKeyDown={(e) => keyDownOnTitle(e, item)}
+              />
               {item.url && (
                 <Link href={item.url} className="text-sky-600 font-light">
                   {item.url}
                 </Link>
               )}
             </div>
-            <p className="font-light">{item.explanation}</p>
+            <textarea
+              className="font-light block w-full"
+              rows={1}
+              defaultValue={item.explanation}
+              onKeyDown={(e) => keyDownOnExplanation(e, item)}
+              onKeyDownCapture={(e) => keyDownCaputureOnExplanation(e)}
+            />
           </div>
         </li>
       ))}

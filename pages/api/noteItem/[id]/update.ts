561e460cf29e2c047058083f35ca066ce2c4aee7
meguro <beautifulchair13@gmail.com>
2023-02-06 22:53:37 +0900

    Fix update

--- pages/api/noteItem/[id]/update.ts
+++ pages/api/noteItem/[id]/update.ts
@@ -6,10 +6,15 @@ export default async function handle(
   res: NextApiResponse
 ) {
   const id = req.query.id as string;
-  const { title, explanation, url } = req.body;
-  const post = await prisma.noteItem.update({
-    where: { id: id },
-    data: { title: title, explanation: explanation, url: url },
-  });
-  res.status(200).json(post);
+  const item = await prisma.noteItem.findFirst({ where: { id: id } });
+  if (item) {
+    const { title, explanation, url } = req.body;
+    const post = await prisma.noteItem.update({
+      where: { id: id },
+      data: { title: title, explanation: explanation, url: url },
+    });
+    res.status(200).json(post);
+  } else {
+    res.status(200).json({});
+  }
 }

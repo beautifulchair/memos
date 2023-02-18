471fa4b7f2d545a8bbabddfbb3663d60e4091fac
meguro <beautifulchair13@gmail.com>
2023-02-18 13:13:35 +0900

    To debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -152,6 +152,7 @@ export default function Home({ dbNoteItems }: PageProps) {
     <ol className="list-decimal mt-10">
       {nis.map((item) => (
         <li key={item.id} className="mt-7 border-b-2 border-dashed pb-3">
+          <div className="text-4xl">{item.id}</div>
           <NoteItem item={item} />
         </li>
       ))}

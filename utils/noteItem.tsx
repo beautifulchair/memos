c69163d0756635a6c4c7f15c41db041b30461f40
meguro <beautifulchair13@gmail.com>
2023-02-27 13:54:59 +0900

    Rename NoteEdit

--- utils/noteItem.tsx
+++ utils/noteItem.tsx
@@ -7,21 +7,18 @@ export type NoteItem = {
 };
 
 const editableprops = ["title", "explanation", "url"] as const;
-type EditableProps = Pick<NoteItem, typeof editableprops[number]>;
+type NoteEdit = Pick<NoteItem, typeof editableprops[number]>;
 
 export function equalItemAtEditable(item1: NoteItem, item2: NoteItem): boolean {
-  type Key = keyof EditableProps;
+  type Key = keyof NoteEdit;
   for (const prop of editableprops) {
     if (item1[prop as Key] != item2[prop as Key]) return false;
   }
   return true;
 }
 
-export function editItem(
-  item: NoteItem,
-  change: Partial<EditableProps>
-): NoteItem {
-  type key = keyof EditableProps;
+export function editItem(item: NoteItem, change: Partial<NoteEdit>): NoteItem {
+  type key = keyof NoteEdit;
   for (const prop in change) {
     item[prop as key] = change[prop as key];
   }
